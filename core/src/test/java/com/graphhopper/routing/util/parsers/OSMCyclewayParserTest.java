68dff98f4522e7d1470fedf6c369e59d04ca2434
Rohit <rohit@sarathy.org>
2024-05-07 03:17:25 -0700

    Add an OSMCyclewayParser unit test

---
+++ core/src/test/java/com/graphhopper/routing/util/parsers/OSMCyclewayParserTest.java
@@ -0,0 +1,31 @@
+package com.graphhopper.routing.util.parsers;
+
+import com.graphhopper.reader.ReaderWay;
+import com.graphhopper.routing.ev.Cycleway;
+import com.graphhopper.routing.ev.EncodedValue;
+import com.graphhopper.routing.ev.EnumEncodedValue;
+import com.graphhopper.storage.IntsRef;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OSMCyclewayParserTest {
+    @Test
+    public void testTags() {
+        EnumEncodedValue<Cycleway> cyclewayAccessEnc =
+            new EnumEncodedValue<>(Cycleway.KEY, Cycleway.class);
+        cyclewayAccessEnc.init(new EncodedValue.InitializerConfig());
+        OSMCyclewayParser parser = new OSMCyclewayParser(cyclewayAccessEnc);
+
+        ReaderWay marketSt = new ReaderWay(1);
+        marketSt.setTag("cycleway:right", "track");
+        marketSt.setTag("highway", "primary");
+        marketSt.setTag("oneway", "yes");
+
+        IntsRef edgeFlags = new IntsRef(1);
+        IntsRef relationFlags = new IntsRef(1);
+
+        parser.handleWayTags(edgeFlags, marketSt, relationFlags);
+        assertEquals(Cycleway.TRACK, cyclewayAccessEnc.getEnum(false, edgeFlags));
+    }
+}
