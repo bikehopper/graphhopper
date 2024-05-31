121aca85f83357bbf3eadd25065973ae70037593
Rohit <rohit@sarathy.org>
2024-05-31 06:31:27 -0700

    Add new OrientationCalculator TagParser subclass.

--- core/src/main/java/com/graphhopper/routing/util/parsers/OrientationCalculator.java
+++ core/src/main/java/com/graphhopper/routing/util/parsers/OrientationCalculator.java
@@ -1,15 +1,11 @@
 package com.graphhopper.routing.util.parsers;
 
-import static com.graphhopper.util.AngleCalc.ANGLE_CALC;
-
 import com.graphhopper.reader.ReaderWay;
 import com.graphhopper.routing.ev.DecimalEncodedValue;
 import com.graphhopper.routing.ev.EncodedValue;
 import com.graphhopper.routing.ev.EncodedValueLookup;
 import com.graphhopper.routing.ev.Orientation;
 import com.graphhopper.storage.IntsRef;
-import com.graphhopper.util.PointList;
-import com.graphhopper.util.shapes.GHPoint3D;
 import java.util.List;
 
 public class OrientationCalculator implements TagParser {
@@ -18,10 +14,6 @@ public class OrientationCalculator implements TagParser {
 
     public OrientationCalculator() {
         this.orientationEnc = Orientation.create();
-    }
-
-    public OrientationCalculator(DecimalEncodedValue orientationEnc) {
-        this.orientationEnc = orientationEnc;
     }
 
     @Override
@@ -33,27 +25,10 @@ public class OrientationCalculator implements TagParser {
     @Override
     public IntsRef handleWayTags(IntsRef edgeFlags, ReaderWay way,
             IntsRef relationFlags) {
-        PointList points = way.getTag("point_list", null);
-        if (points == null) {
-            System.out.println("WARN: no points found for way=" + way.getId());
-        } else {
-            // Use the end of the edge to calculate a way's orientation.
-            GHPoint3D point1 = points.get(points.size() - 2);
-            GHPoint3D point2 = points.get(points.size() - 1);
-            double orientationRad = ANGLE_CALC.calcOrientation(
-                    point1.getLat(), point1.getLon(),
-                    point2.getLat(), point2.getLon());
-            orientationEnc.setDecimal(false, edgeFlags, orientationRad);
 
-            // Use the beginning of the edge to calculate a way's orientation
-            // for the reverse direction.
-            GHPoint3D point1Rev = points.get(1);
-            GHPoint3D point2Rev = points.get(0);
-            double orientationRevRad = ANGLE_CALC.calcOrientation(
-                    point1Rev.getLat(), point1Rev.getLon(),
-                    point2Rev.getLat(), point2Rev.getLon());
-            orientationEnc.setDecimal(true, edgeFlags, orientationRevRad);
-        }
-        return edgeFlags;
+        // 1. Get coordinates
+        // 2. Use AngleCalc.getOrientation()
+        // 3. Set OrientationEnc to the result of (2)
+        return null;
     }
 }
