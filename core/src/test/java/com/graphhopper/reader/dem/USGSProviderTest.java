b54024c59fba50013b39cd88ebefebc312e0a606
Rohit <rohit@sarathy.org>
2024-06-17 15:52:19 -0700

    Add more testFilename() assertions.

--- core/src/test/java/com/graphhopper/reader/dem/USGSProviderTest.java
+++ core/src/test/java/com/graphhopper/reader/dem/USGSProviderTest.java
@@ -5,16 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class USGSProviderTest {
+
+    USGSProvider provider = new USGSProvider("");
+
     @Test
     public void testFilename() {
-        USGSProvider provider = new USGSProvider("");
         assertEquals("ned19_n38x00w122x50",
-                provider.getFileName(38.00, -122.50));
+                provider.getFileName(38, -122.5));
+        assertEquals("ned19_n37x75w122x00",
+                provider.getFileName(37, -122));
     }
 
     @Test
     public void testMinLat() {
-        USGSProvider provider = new USGSProvider("");
         assertEquals(37.75, provider.getMinLatForTile(37.99));
         assertEquals(38, provider.getMinLatForTile(38));
     }
