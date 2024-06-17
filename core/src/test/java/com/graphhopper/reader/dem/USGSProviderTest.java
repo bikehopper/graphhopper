5bef3c606c8e0002ea490e9cd7ff57508fb733c1
Rohit <rohit@sarathy.org>
2024-06-17 16:11:43 -0700

    Fix incorrect unit tests!

--- core/src/test/java/com/graphhopper/reader/dem/USGSProviderTest.java
+++ core/src/test/java/com/graphhopper/reader/dem/USGSProviderTest.java
@@ -1,6 +1,6 @@
 package com.graphhopper.reader.dem;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,16 +9,23 @@ class USGSProviderTest {
     USGSProvider provider = new USGSProvider("");
 
     @Test
-    public void testFilename() {
-        assertEquals("ned19_n38x00w122x50",
-                provider.getFileName(38, -122.5));
-        assertEquals("ned19_n37x75w122x00",
-                provider.getFileName(37, -122));
+    public void testMinLat() {
+        assertEquals(37.75, provider.getMinLatForTile(37.76));
+        assertEquals(37.5, provider.getMinLatForTile(37.75));
+        assertEquals(37.5, provider.getMinLatForTile(37.74));
     }
 
     @Test
-    public void testMinLat() {
-        assertEquals(37.75, provider.getMinLatForTile(37.99));
-        assertEquals(38, provider.getMinLatForTile(38));
+    public void testMinLon() {
+        assertEquals(-122.5, provider.getMinLonForTile(-122.26));
+        assertEquals(-122, provider.getMinLonForTile(-122));
+    }
+
+    @Test
+    public void testFilename() {
+        assertEquals("ned19_n38x00w122x50",
+                provider.getFileName(38, -122.5));
+        assertEquals("ned19_n38x00w122x50",
+                provider.getFileName(37.76, -122.26));
     }
 }
