69d614890757134b627cd70ab316e35776b47c47
Rohit <rohit@sarathy.org>
2024-12-20 15:21:07 -0800

    Add a TurnCostsConfig object

---
+++ core/src/main/java/com/graphhopper/routing/util/TurnCostsConfig.java
@@ -0,0 +1,36 @@
+package com.graphhopper.routing.util;
+
+public class TurnCostsConfig {
+    private double leftCostsSeconds = 45;
+    private double leftSharpCostsSeconds = 45;
+    private double rightCostsSeconds = 5;
+    private double rightSharpCostsSeconds = 15;
+
+    private double uTurnCostsSeconds = 60;
+
+    private double minAngleDegrees = 25, minSharpAngleDegrees = 80, minUTurnAngleDegrees = 180;
+
+    public double getLeftCostsSeconds() {
+        return leftCostsSeconds;
+    }
+
+    public double getLeftSharpCostsSeconds() {
+        return leftSharpCostsSeconds;
+    }
+
+    public double getRightCostsSeconds() {
+        return rightCostsSeconds;
+    }
+
+    public double getRightSharpCostsSeconds() {
+        return rightSharpCostsSeconds;
+    }
+
+    public double getMinAngleDegrees() {
+        return minAngleDegrees;
+    }
+
+    public double getUTurnCostsSeconds() {
+        return uTurnCostsSeconds;
+    }
+}
