3842bab275a40d5dbdfe422fd7215366b51cace0
Arindam Bose <arindam.1993@gmail.com>
2025-09-26 08:23:34 -0700

    add node version detection

--- coolify/install.sh
+++ coolify/install.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 22
-nvm use 22
+node_version=$(node -v | grep -Eo '[0-9]+' | head -1)
+# detect and install node version if necessary
+if [ $node_version -ne 22 ]
+then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    nvm install 22
+    nvm use 22
+fi
 npx --yes @bikehopper/data-mirror # download data
 mvn clean install -DskipTests # install dependencies
