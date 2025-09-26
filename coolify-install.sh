3c5a773ccd2f25eadb6e28c08b5ef10ca078e598
Arindam Bose <arindam.1993@gmail.com>
2025-09-25 21:20:01 -0700

    install nvm

--- coolify-install.sh
+++ coolify-install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+source ~/.bashrc
+nvm install 22
+nvm use 22
 npx --yes @bikehopper/data-mirror # download data
 mvn clean install -DskipTests # install dependencies
