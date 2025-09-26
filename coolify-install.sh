f246738ec0245fb5fec5d38ce462ce82d37b42b1
Arindam Bose <arindam.1993@gmail.com>
2025-09-25 21:24:53 -0700

    tryhard nvm fix

--- coolify-install.sh
+++ coolify-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 22
 nvm use 22
 npx --yes @bikehopper/data-mirror # download data
