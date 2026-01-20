100a23551ec7f4a05e4e65b3b7c5dd3c601aabf1
Aditya Bhumbla <adit.bhumbla@gmail.com>
2026-01-19 18:09:55 -0800

    Tile ways output to PMTiles and serve

--- coolify/build.sh
+++ coolify/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 java -Xmx8g -jar web/target/graphhopper-web-*.jar import bay-area/config.yml
+npx @bikehopper/node-tippecanoe -zg -Z8 -l ways_dump -P -o web-bundle/src/main/resources/com/graphhopper/maps/ways.pmtiles --drop-densest-as-needed logs/ways_dump.ldgeojson
