257fc2b04b9191a166c094b3d103e81043b3e374
Aditya Bhumbla <adit.bhumbla@gmail.com>
2026-01-19 18:44:49 -0800

    Remove build.sh

--- coolify/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-nvm use 22
-java -Xmx8g -jar web/target/graphhopper-web-*.jar import bay-area/config.yml
-npx @bikehopper/node-tippecanoe -zg -Z8 -l ways_dump -P -o web-bundle/src/main/resources/com/graphhopper/maps/ways.pmtiles --drop-densest-as-needed logs/ways_dump.ldgeojson
