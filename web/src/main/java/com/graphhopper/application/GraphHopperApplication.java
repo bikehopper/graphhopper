bec5768a9175e9179f2a72666c938c65a31a473c
Arindam Bose <arindam.1993@gmail.com>
2026-01-31 17:19:06 -0800

    dump to shared volume to prevent overlay2 bloat

--- web/src/main/java/com/graphhopper/application/GraphHopperApplication.java
+++ web/src/main/java/com/graphhopper/application/GraphHopperApplication.java
@@ -45,7 +45,6 @@ public final class GraphHopperApplication extends Application<GraphHopperServerC
         bootstrap.addCommand(new ImportCommand());
         bootstrap.addCommand(new MatchCommand());
         bootstrap.addBundle(new AssetsBundle("/com/graphhopper/maps/", "/maps/", "index.html"));
-        bootstrap.addBundle(new AssetsBundle("/debug/", "/debug-tiles/", "ways.pmtiles", "debug-tiles"));
         // see this link even though its outdated?! // https://www.webjars.org/documentation#dropwizard
         bootstrap.addBundle(new AssetsBundle("/META-INF/resources/webjars", "/webjars/", null, "webjars"));
     }
