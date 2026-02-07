700e4de8dfc26d7ed169f5e735fe7a8e738c749d
Arindam Bose <arindam.1993@gmail.com>
2026-02-06 23:14:03 -0800

    update tippecanoe params

--- coolify/install.sh
+++ coolify/install.sh
@@ -29,5 +29,5 @@ mvn clean install -DskipTests # install dependencies
 
 # build graph
 java -Xmx8g -jar web/target/graphhopper-web-*.jar import bay-area/config.yml
-npx @bikehopper/node-tippecanoe -zg -Z8 -l ways_dump -P -o web-bundle/src/main/resources/debug/ways.pmtiles --drop-densest-as-needed /tmp/graphhopper/ways_dump.ldgeojson
+npx @bikehopper/node-tippecanoe -z14 -Z8 --maximum-tile-bytes=1572864 -l ways_dump -P -o web-bundle/src/main/resources/debug/ways.pmtiles --drop-densest-as-needed /tmp/graphhopper/ways_dump.ldgeojson
 rm /tmp/graphhopper/ways_dump.ldgeojson
