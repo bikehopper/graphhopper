295b4fdc00469fd1601b47dbacbaefd043c00013
Arindam Bose <arindam.1993@gmail.com>
2026-02-07 16:19:40 -0800

    Fix OOM's caused by weird Jax-rs stuff

--- web-bundle/src/main/java/com/graphhopper/resources/PMTilesResource.java
+++ web-bundle/src/main/java/com/graphhopper/resources/PMTilesResource.java
@@ -11,10 +11,8 @@ import org.slf4j.LoggerFactory;
 import com.graphhopper.GraphHopper;
 import com.graphhopper.routing.util.EncodingManager;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.RandomAccessFile;
 
 @Path("/pmtiles")
 public class PMTilesResource {
@@ -22,14 +20,14 @@ public class PMTilesResource {
     private static String mimeType = "application/vnd.pmtiles";
     private static String pmtilesPath = "web-bundle/src/main/resources/debug/ways.pmtiles";
     private static final Logger logger = LoggerFactory.getLogger(PMTilesResource.class);
-    private File file;
-    private byte[] data;
+    private static RandomAccessFile file;
 
     @Inject
     public PMTilesResource(GraphHopper graphHopper, EncodingManager encodingManager) {
-        this.file = new File(pmtilesPath);
         try {
-            this.data = Files.readAllBytes(Paths.get(this.file.getAbsolutePath()));
+            if (file == null) {
+                file = new RandomAccessFile(pmtilesPath, "r");
+            }
         } catch (IOException e) {
             logger.warn(pmtilesPath + "file not present");
         }
@@ -38,46 +36,53 @@ public class PMTilesResource {
     @GET
     @Path("/ways.pmtiles")
     public Response getFile( @HeaderParam("Range") String range) throws IOException {
-        if (!this.file.exists() || this.data == null) {
+        if (file == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
+        // Assume full file
+        long start = 0;
+        long end = file.length();
+        boolean partial = range != null;
 
-        if (range == null) {
-            // Return the full file if no range header is provided
-            ResponseBuilder response = Response.ok((Object) this.file);
-            response.header("Content-Type", mimeType);
-            response.header("Content-Length", this.file.length());
-            response.header("Accept-Ranges", "bytes"); // Indicate that the server accepts range requests
-            return response.build();
-        } else {
-            // Handle the range request
-            try {
-                String[] ranges = range.substring("bytes=".length()).split("-");
-                long start = Long.parseLong(ranges[0]);
-                long end = ranges.length > 1 ? Long.parseLong(ranges[1]) : this.file.length() - 1;
+        ResponseBuilder res;
+        // Narrow down range if its specified
+        if (partial) {
+            String[] ranges = range.substring("bytes=".length()).split("-");
+            start = Long.parseLong(ranges[0]);
 
-                if (start > this.file.length() || end >= this.file.length()) {
-                     // 416 Requested Range Not Satisfiable
-                    return Response.status(Response.Status.REQUESTED_RANGE_NOT_SATISFIABLE)
-                            .header("Content-Range", "bytes */" + file.length())
-                            .build();
+            // if end is specified set to it
+            if (ranges.length > 1) {
+                long potentialEnd = Long.parseLong(ranges[1]);
+                if (potentialEnd < end) {
+                    end = potentialEnd;
                 }
-
-                // Read the specified byte range from the file
-                
-                byte[] partialData = new byte[(int) (end - start + 1)];
-                System.arraycopy(data, (int) start, partialData, 0, partialData.length);
-
-                // Return partial content (HTTP 206)
-                ResponseBuilder response = Response.status(Response.Status.PARTIAL_CONTENT)
-                        .entity(partialData)
-                        .header("Content-Range", "bytes " + start + "-" + end + "/" + file.length())
-                        .header("Content-Length", partialData.length)
-                        .header("Content-Type", mimeType); // Set correct MIME type
-                return response.build();
-            } catch (NumberFormatException | IndexOutOfBoundsException e) {
-                return Response.status(Response.Status.REQUESTED_RANGE_NOT_SATISFIABLE).build();
             }
+
+            res = Response.status(Response.Status.PARTIAL_CONTENT);
+
+        } else {
+            res = Response.ok();
         }
+
+        file.seek(start);
+
+        int length = (int) (end - start);
+        byte[] data = new byte[length];
+
+        // Read the data into the buffer
+        file.read(data);
+        res.entity(data);
+
+        // Set headers
+        res.header("Content-Length", length);
+        res.header("Content-Type", mimeType);
+
+        if (partial) {
+            res.header("Content-Range", "bytes " + start + "-" + end + "/" + file.length());
+        } else {
+           res.header("Accept-Ranges", "bytes"); // Indicate that the server accepts range requests
+        }
+
+        return res.build();
     }
 }
